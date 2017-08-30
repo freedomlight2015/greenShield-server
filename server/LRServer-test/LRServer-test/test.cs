be1ce08088ce7316abda7fe19ef5cc4cd95d41cf
戴玉辉 <2900762188@qq.com>
2017-08-30 19:59:04 +0800

    modify

---
+++ server/LRServer-test/LRServer-test/test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRServer_test
+{
+    internal class test
+    {
+        public enum GEnum0 : long
+        {
+            All = 0x7fffffffffL,
+            const_0 = 0L,
+            const_1 = 1L,
+            const_10 = 0x200L,
+            const_11 = 0x400L,
+            const_12 = 0x800L,
+            const_13 = 0x1000L,
+            const_14 = 0x2000L,
+            const_15 = 0x4000L,
+            const_16 = 0x8000L,
+            const_17 = 0x10000L,
+            const_18 = 0x20000L,
+            const_19 = 0x40000L,
+            const_2 = 2L,
+            const_20 = 0x80000L,
+            const_21 = 0x100000L,
+            const_22 = 0x200000L,
+            const_23 = 0x400000L,
+            const_24 = 0x800000L,
+            const_25 = 0x1000000L,
+            const_26 = 0x2000000L,
+            const_27 = 0x4000000L,
+            const_28 = 0x8000000L,
+            const_29 = 0x10000000L,
+            const_3 = 4L,
+            const_30 = 0x20000000L,
+            const_31 = 0x40000000L,
+            const_32 = 0x80000000L,
+            const_33 = 0x100000000L,
+            const_34 = 0x200000000L,
+            const_35 = 0x400000000L,
+            const_36 = 0x800000000L,
+            const_37 = 0x1000000000L,
+            const_38 = 0x2000000000L,
+            const_39 = 0x4000000000L,
+            const_4 = 8L,
+            const_5 = 0x10L,
+            const_6 = 0x20L,
+            const_7 = 0x40L,
+            const_8 = 0x80L,
+            const_9 = 0x100L
+        }
+    }
+}
